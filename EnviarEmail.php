a318234e026fd3d01b5df9bc34124cfea9a822c4
Alan Vinicius <alan123vinicius@gmail.com>
2020-09-17 01:50:06 -0300

    Add files via upload

---
+++ EnviarEmail.php
@@ -0,0 +1,47 @@
+<?php
+
+require_once('vendor/phpmailer/phpmailer/src/PHPMailer.php');
+require_once('vendor/phpmailer/phpmailer/src/SMTP.php');
+require_once('vendor/phpmailer/phpmailer/src/Exception.php');
+require_once("vendor/autoload.php");
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+
+class EnviarEmail 
+{
+    
+    public $Nome = "";
+    public $Email = "";
+    public $Menssagem = "";
+
+    public function EnviarEmail () 
+    {
+        $Enviar = new PHPMailer(true);
+
+        $email = new PHPMailer(true);
+        
+        $Enviar->SMTPDebug = 2;               
+        $Enviar->isSMTP();
+        $Enviar->Host = '';  
+        $Enviar->SMTPAuth = true;             
+        $Enviar->Username = '';
+        $Enviar->Password = '';     
+        $Enviar->SMTPSecure = '';               
+        $Enviar->Port = ''; 
+        $Enviar->setFrom('');
+        $Enviar->addAddress('','');
+        $Enviar->Subject = "";
+        $Enviar->Body = "de: {$this->Nome}\nemail:{$this->Email}\nmenssagem: {$this->menssagem}";
+        
+        if($email->send())
+        {
+            echo "Email enviado com sucesso";
+        }else
+        {
+            echo "Erro ao enviar email";
+        }
+    }
+}
+?>
