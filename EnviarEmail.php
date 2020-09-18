9f9f776d8b4819897eb423619e327e9d18157d5a
Alan Vinicius <alan123vinicius@gmail.com>
2020-09-17 22:12:20 -0300

    Update EnviarEmail.php

--- EnviarEmail.php
+++ EnviarEmail.php
@@ -1,38 +1,45 @@
 <?php
 
+/*Requisições*/
 require_once('vendor/phpmailer/phpmailer/src/PHPMailer.php');
 require_once('vendor/phpmailer/phpmailer/src/SMTP.php');
 require_once('vendor/phpmailer/phpmailer/src/Exception.php');
 require_once("vendor/autoload.php");
 
+/*Use*/
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 use PHPMailer\PHPMailer\SMTP;
 
+/*Classe*/
 class EnviarEmail 
 {
+    //Atributos
+    public $Nome = "localhost";
+    public $Email = "localhost@gmail.com";
+    public $Menssagem = "Uma menssagem enviado de localhost";
     
-    public $Nome = "";
-    public $Email = "";
-    public $Menssagem = "";
-
+    /*Função responsavel por enviar email*/
     public function EnviarEmail () 
     {
+        //Instancia o objeto e executa o método da classe php mailer.
         $Enviar = new PHPMailer(true);
         
+        //Acessa os valores aos atributos da classe php mailer.
         $Enviar->SMTPDebug = 2;               
         $Enviar->isSMTP();
-        $Enviar->Host = '';  
+        $Enviar->Host = 'smtp.gmail.com';  
         $Enviar->SMTPAuth = true;             
-        $Enviar->Username = '';
-        $Enviar->Password = '';     
-        $Enviar->SMTPSecure = '';               
-        $Enviar->Port = ''; 
-        $Enviar->setFrom('');
-        $Enviar->addAddress('','');
-        $Enviar->Subject = "";
-        $Enviar->Body = "de: {$this->Nome}\nemail:{$this->Email}\nmenssagem: {$this->menssagem}";
+        $Enviar->Username = 'Seu email gmail';
+        $Enviar->Password = 'sua senha gmail ou de app';     
+        $Enviar->SMTPSecure = 'tls';               
+        $Enviar->Port = 587; 
+        $Enviar->setFrom('localhost@gmail.com');
+        $Enviar->addAddress('Email do destinario','Nome do destinario');
+        $Enviar->Subject = "Assunto";
+        $Enviar->Body = "de: {$this->Nome}\nemail:{$this->Email}\nmenssagem: {$this->Menssagem}";
         
+        //Instrução condicional que executa o envio do email se os dados informados estiverem corretos ou exibe a menssagem error.
         if($Enviar->send())
         {
             echo "Email enviado com sucesso";
@@ -42,4 +49,6 @@ class EnviarEmail
         }
     }
 }
+// Instancia o objeto e executa o método EnviarEmail da classe EnviarEmail.
+$execute = new EnviarEmail();
 ?>
